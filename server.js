00956ded0192498333003f41968bd6be20c4ec44
Mike Semko <msemko@gmail.com>
2017-02-13 04:06:59 -0500

    send index.html for any invalid route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ let express = require("express");
 let app = express();
 const PORT = process.env.NODE_PORT || 3000;
 app.use(express.static("public"));
+app.get("*", (req, res) => {
+	res.sendFile(__dirname + "/public/index.html");
+});
 app.listen(PORT, () => {
 	console.log(`Server active on port ${PORT}`);
 });
