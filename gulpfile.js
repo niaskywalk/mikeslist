3c8494d39b635221fb477d909a33409a91204c01
Mike Semko <msemko@gmail.com>
2017-02-13 05:21:10 -0500

    create css task and wire it up to the watcher

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 let gulp = require("gulp");
 let nodemon = require("gulp-nodemon");
 let browserSync = require("browser-sync").create();
+let plumber = require("gulp-plumber");
+let sourceMaps = require("gulp-sourcemaps");
+let sass = require("gulp-sass");
+let autoprefixer = require("gulp-autoprefixer");
 const BROWSER_SYNC_DELAY = 400;
 
 gulp.task("nodemon", () => {
@@ -21,6 +25,22 @@ gulp.task("browser-sync", ["nodemon"], () => {
 	});
 });
 
+gulp.task("css", () => {
+	return gulp.src("public/scss/main.scss")
+		.pipe(plumber(function(error) {
+			browserSync.notify('<pre style="text-align: left">' + error.message + '</pre>', 4000);
+			this.emit("end");
+		}))
+		.pipe(sourceMaps.init())
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(gulp.dest("public/css/"))
+		.pipe(browserSync.stream())
+		.pipe(sourceMaps.write("."))
+		.pipe(gulp.dest("public/css/"));
+});
+
 gulp.task("default", ["browser-sync"], () => {
 	gulp.watch(["public/**/*.html", "public/**/*.tpl", "public/js/main.js"], browserSync.reload);
+	gulp.watch("public/scss/**/*.scss", ["css"]);
 });
