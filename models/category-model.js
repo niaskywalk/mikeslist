87a6feb96ffdf06e6d3d38b1c36ff47a3dc113f0
Mike Semko <msemko@gmail.com>
2017-02-22 02:01:11 -0500

    Add virtual field to 'listingCount' to category schema. Add reference field to listings. Add comments to both model files.

--- models/category-model.js
+++ models/category-model.js
@@ -8,8 +8,19 @@ let categorySchema = new mongoose.Schema({
 		lowercase: true,
 		trim: true,
 		required: [true, "Category name may not be blank"]
-	}
+	},
+	listings: [{type: String, ref: "Listing"}]
 });
+
+//create a virtual field to report the number of listings in a category
+categorySchema.virtual("listingCount").get(function() {
+	return this.listings.length;
+});
+
+//ensure that the virtual fields are included when document is sent back
+//via json or used as object
+categorySchema.set("toObject", {virtuals: true});
+categorySchema.set("toJSON", {virtuals: true});
 
 let Category = mongoose.model("Category", categorySchema);
 
