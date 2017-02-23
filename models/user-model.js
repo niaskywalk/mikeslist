aa0083a29b60554225725baaaf4fa9ace4422013
Mike Semko <msemko@gmail.com>
2017-02-23 15:20:19 -0500

    create user model and install bcrypt hooks

---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+"use strict";
+
+let mongoose = require("mongoose");
+let bcrypt = require("bcrypt");
+let UserSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		required: [true, "User must have an email"]
+	},
+	password: String
+});
+
+UserSchema.methods.verifyPassword = function(password){
+	return bcrypt.compare(password, this.password);
+};
+
+UserSchema.pre("save", function(next){
+	let user = this;
+	const SALT_ROUNDS = 10;
+	if (!user.isModified("password")){
+		return next();
+	}
+	bcrypt.hash(user.password, SALT_ROUNDS).
+	then(hash => {
+		user.password = hash;
+	});
+});
